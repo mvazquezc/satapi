ae9a2ce0c388489504a786b90c066c31f77cddf2
Mario Vazquez <mavazque@redhat.com>
2017-03-23 14:39:24 +0100

    Fixed bug when enabling repos and sending releasever/basearch as None

--- satapi/satapilib/products.py
+++ satapi/satapilib/products.py
@@ -21,23 +21,19 @@ class SatAPIProducts(SatAPIConnection):
 
     # Enable repository set from a product
     def enableRepositorySet(self, ProductId, RepositoryId, Basearch=None, Releasever=None):
-        JSONData=json.dumps(
-            {
-                'basearch': Basearch,
-                'releasever': Releasever
-            }
-        )
+        JSONData={'basearch': Basearch, 'releasever': Releasever}
+        # Remove empty keys from json data, some repos doesn't have releasever or basearch thus the action will fail if those values are sent as None
+        JSONData = dict((k, v) for k, v in JSONData.iteritems() if v)
+        JSONData = json.dumps(JSONData)
         Response=self.PUT(self.KatelloAPILocation + 'products/' + str(ProductId) + '/repository_sets/' + str(RepositoryId) + '/enable', JSONData)
         return Response
 
     # Disable repository set from a product
     def disableRepositorySet(self, ProductId, RepositoryId, Basearch=None, Releasever=None):
-        JSONData=json.dumps(
-            {
-                'basearch': Basearch,
-                'releasever': Releasever
-            }
-        )
+        JSONData={'basearch': Basearch, 'releasever': Releasever}
+        # Remove empty keys from json data, some repos doesn't have releasever or basearch thus the action will fail if those values are sent as None
+        JSONData = dict((k, v) for k, v in JSONData.iteritems() if v)
+        JSONData = json.dumps(JSONData)
         Response=self.PUT(self.KatelloAPILocation + 'products/' + str(ProductId) + '/repository_sets/' + str(RepositoryId) + '/disable', JSONData)
         return Response
 
