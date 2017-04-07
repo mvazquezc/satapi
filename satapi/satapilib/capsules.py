88b6407d827857ec97c67f869b58928a1ce505b5
Mario Vazquez <mavazque@redhat.com>
2017-04-07 23:25:24 +0200

    Added capsule sync, set location and organization for capsules

--- satapi/satapilib/capsules.py
+++ satapi/satapilib/capsules.py
@@ -24,4 +24,33 @@ class SatAPICapsules(SatAPIConnection):
         Response=self.GET(self.KatelloAPILocation + 'capsules/',
                             {'search': criteria, 'count': count, 'per_page': count})
         return Response
+   
+    # Synchronize a capsule
+    def syncCapsule(self, Id):
+        Response=self.POST(self.KatelloAPILocation + 'capsules/%s/content/sync' %str(Id))
+        return Response
+
+    # Set Capsule Locations
+    def setCapsuleLocations(self, Id, LocationsIds):
+        JSONData=json.dumps(
+            {
+                'smart_proxy': {
+                    'location_ids': LocationsIds
+                }
+            }
+        )
+        Response=self.PUT(self.SatAPILocation + 'smart_proxies/' + str(Id), JSONData)
+        return Response
     
+    # Set Capsule organizations
+    def setCapsuleOrganizations(self, Id, OrganizationIds):
+        JSONData=json.dumps(
+            {
+                'smart_proxy': {
+                    'organization_ids': OrganizationIds
+                }
+            }
+        )
+        Response=self.PUT(self.SatAPILocation + 'smart_proxies/' + str(Id), JSONData)
+        return Response
+
